7a172fb2f01e3cde03964aa4cd9afa267c835161
AstridW <AstridWcountee@gmail.com>
2014-10-08 22:33:41 -0500

    blackjack with some bugs

--- blackjack4.rb
+++ blackjack4.rb
@@ -1,3 +1,23 @@
+
+
+
+class Card
+
+  attr_accessor :suit
+  attr_accessor :rank
+  attr_accessor :value
+
+  def initialize(suit,rank,value)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+
+end
+
+
+
+
 print  "Hi what is your name?"
 @name = gets.chomp
 
@@ -14,21 +34,6 @@ puts "------------------------------------------------------------------------"
 puts "------------------------------------------------------------------------"
 
 puts "Ready!"
-
-
-class Card
-
-  attr_accessor :suit
-  attr_accessor :rank
-  attr_accessor :value
-
-  def initialize(suit,rank,value)
-    @suit = suit
-    @rank = rank
-    @value = value
-  end
-end
-
 
 
 
@@ -107,52 +112,157 @@ end
   player_hand = (new_deck.shift.value + new_deck.shift.value)
 
 
+
+
   cards_in_play = true
 
 
 
+  # puts "Hit or stay? (h/s) or end (exit)"
+  # answer = gets.chomp
+# if answer  == "h"
+#     player_hand += new_deck.shift.value
+#     puts "#{@name} has #{player_hand}"
+#
+# elsif answer == "s"
+#     dealer_hand += new_deck.shift.value until dealer_hand >= 16
+#       puts "Dealer has #{dealer_hand}"
+#       exit
+# elsif answer == "exit"
+#     exit
+#   end
+# end
+# end
+#
+#
+#
+# if player_hand == 21 && dealer_hand == 21
+#   puts "You both tied with 21"
+# elsif dealer_hand == 21
+#   puts "Dealer Won. You Lost with #{player_hand}"
+# elsif player_hand == 21
+#   puts "You Won with #{player_hand}!"
+# elsif player_hand > 21
+#   puts "You Lost with #{player_hand}"
+# elsif dealer_hand > 21
+#   puts "Dealer over - YOU WON"
+# elsif dealer_hand >= player_hand
+#   puts "Dealer Won"
+# elsif player_hand >= dealer_hand
+#   puts "You Won with #{player_hand}"
+# end
 
 
-if player_hand == 21 && dealer_hand == 21
-  puts "Its a Draw! Push!"
-  continue_in_play = false
-elsif dealer_hand == 21
-  puts "You Lose Dealer has #{dealer_hand}"
-  continue_in_play = false
-elsif player_hand == 21
-  puts "You Won, BlackJack! your cards add up to #{player_hand}!"
-  continue_in_play = false
-elsif player_hand > 21
-  puts "You Went Over 21, You Lose, your cards add up to #{player_hand}"
-  continue_in_play = false
-elsif dealer_hand > 21
-  puts "Dealer Went Over 21 - You Win!"
-  continue_in_play = false
-elsif dealer_hand >= player_hand
-  puts "Sorry You Lose"
-  continue_in_play = false
-elsif player_hand >= dealer_hand
-  puts "You Won, your cards add up to #{player_hand}"
-  continue_in_play = false
-end
 
-
-
+# # my first atttempt
+#
 while cards_in_play
   puts "The dealer has #{dealer_hand}"
   puts "#{@name} has #{player_hand}"
+
+until dealer_hand >= 21 || player_hand >= 21 do
   puts "Hit or stay? (h/s)"
   answer = gets.chomp
-    if answer == "h"
+    if answer == "h" && player_hand < 21
         player_hand += new_deck.shift.value
-    else
-        continue_in_play = false
-    end
-    if player_hand > 21
-       continue_to_play = false
-    end
-    end
-
-    while dealer_hand.value <= 16
+        dealer_hand += new_deck.shift.value unless
+        dealer_hand > 16
+        puts "The dealer has #{dealer_hand}"
+        puts "#{@name} has #{player_hand}"
+    elsif answer == "h" && player_hand > dealer_hand && dealer_hand > 16
+          puts "#{@name} wins you have #{player_hand}"
+    elsif answer == "h" && player_hand == 21
+        puts "You Won, BlackJack! your cards add up to #{player_hand}!"
+        cards_in_play = false
+    elsif answer == "h" && player_hand > 21
+        puts "You Went Over 21, You Lose, your cards add up to #{player_hand}"
+        cards_in_play = false
+     elsif answer == "h" &&  player_hand == 21 && dealer_hand == 21
+        puts "Its a Draw! Push!"
+        cards_in_play = false
+    elsif answer == "s" && dealer_hand < 16
         dealer_hand += new_deck.shift.value
+    elsif dealer_hand >= player_hand
+        puts "Sorry You Lose"
+        cards_in_play = false
+    elsif answer == "s" && player_hand > dealer_hand
+        puts "you win"
+    elsif   dealer >21
+        puts "Dealer Went Over 21 - You Win!"
+        cards_in_play = false
+    elsif answer == "exit"
+      exit
     end
+  end
+end
+
+
+    # if player_hand > 21
+    #    cards_in_play = false
+    # end
+    #
+#
+#
+#
+#
+#
+#   while cards_in_play
+#     if player_hand == 21 && dealer_hand == 21
+#       puts "Its a Draw! Push!"
+#       cards_in_play = false
+#     elsif dealer_hand == 21
+#       puts "You Lose Dealer has #{dealer_hand}"
+#       cards_in_play = false
+#     elsif player_hand == 21
+#       puts "You Won, BlackJack! your cards add up to #{player_hand}!"
+#       cards_in_play = false
+#     # elsif player_hand > 21
+#     #   puts "You Went Over 21, You Lose, your cards add up to #{player_hand}"
+#     #   cards_in_play = false
+#     elsif dealer_hand > 21
+#       puts "Dealer Went Over 21 - You Win!"
+#       cards_in_play = false
+#     elsif dealer_hand >= player_hand
+#       puts "Sorry You Lose"
+#       cards_in_play = false
+#     elsif player_hand >= dealer_hand
+#       puts "You Won, your cards add up to #{player_hand}"
+#       cards_in_play = false
+#     end
+#   end
+#
+#
+#     if cards_in_play == false
+#       puts "#{@name}, want to play again? (y/n)"
+#       answer = gets.chomp
+#         if answer == "y"
+#           play = Play.new
+#         else
+#           continue_in_play = false
+#             puts "Game over#{@name}, Thanks for playing!"
+#           end
+#         end
+# end
+
+
+#
+#
+#
+# while cards_in_play
+#   puts "The dealer has #{dealer_hand}"
+#   puts "#{@name} has #{player_hand}"
+#   puts "Hit or stay? (h/s)"
+#   answer = gets.chomp
+#     if answer == "h"
+#         player_hand += new_deck.shift.value
+#     else
+#         cards_in_play = false
+#     end
+#     if player_hand > 21
+#        cards_in_play = false
+#     end
+#     end
+#
+#     while dealer_hand <= 16
+#         dealer_hand += new_deck.shift.value
+#     end
